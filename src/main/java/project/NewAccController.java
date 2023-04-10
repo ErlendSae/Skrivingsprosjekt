bdc973a3c08b7aa47c8e33f8649f0285d504c96c
Erlend L Saeveraas <erlenlsa@stud.ntnu.no>
2023-04-10 17:31:15 +0200

    statrtet på user-klassen

--- src/main/java/project/NewAccController.java
+++ src/main/java/project/NewAccController.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class NewAccController extends StartpageController{
     @FXML
-    private TextField inpText, inpPassword1, inpPassword2;
+    private TextField inpText;
+    @FXML
+    private PasswordField inpPassword1, inpPassword2;
     @FXML
     private Hyperlink logInBtn2, gameLink;
     @FXML
--- src/main/java/project/User.java
+++ src/main/java/project/User.java
@@ -1,5 +1,24 @@
 package project;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class User {
-    
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    private Score pb;   //personlig beste score til denne useren
+
+    public User(TextField username, PasswordField password){
+        this.username = username;
+        this.password = password;
+    }
+    public Score updatePb(Score nyPb){
+        this.pb = nyPb;
+        return this.pb;
+    }
+
 }
